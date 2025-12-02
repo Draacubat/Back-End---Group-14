7b02ac936b4ad31ee8b395fe63fd7aa706ac7489
Siri <sirppesipuli94@outlook.com>
2025-12-02 15:36:41 +0200

    Added structure for book-models

--- src/models/books.js
+++ src/models/books.js
@@ -5,5 +5,21 @@ import mongoose from "mongoose";
 const { Schema  } = mongoose;
 
 const booksSchema = new Schema(
-    
-)
+    {
+        name: {
+            first: {
+                type: String,
+                trim: true,
+            },
+            last: {
+                type: String,
+                trim: true
+            },
+        },
+        title: {
+            type: String
+        }
+    }
+);
+
+export const Books = mongoose.model("Books", userSchema);
