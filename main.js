58b5b6c28de7fe7734d55292be5bbb62ad421719
Rokolampi <144310397+Rokolampi@users.noreply.github.com>
2025-12-03 12:56:22 +0200

    Layout & css
    Added layout and initial styling for the homepage + added some stuff to main.js

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 "use strict";
 
+
 import express from "express";
+import expressLayouts from "express-ejs-layouts";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
+
+app.use(expressLayouts);
+app.set("layout", "layouts/main");
+
 app.set("view engine", "ejs");
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 app.set("views", path.join(__dirname, "src", "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("port", process.env.PORT || 3000);
 
-// Testireitti
+// Middleware makes 'user' variable available in all views
+app.use((req, res, next) => {
+  res.locals.user = null; // not logged in
+  next();
+});
+
+// Render index.ejs template
 app.get("/", (req, res) => {
-  res.render("index", { message: "Hello World!" });
+  res.render("index");
 });
 
 // Run server
