8016f7741f60cd6a79c1e6ee2c7c987801cd7e13
Rokolampi <144310397+Rokolampi@users.noreply.github.com>
2025-12-15 16:17:22 +0200

    Books/MyBooks routes and controllers updated
    Updated Books controllers and routes and added loan book & return book functions

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { booksSchema as Books } from "../models/books.js";
+import { Books } from "../models/books.js";
 
 //Shows search results (renders to availableBooks.ejs)
 const searchBooks = async (req, res) => {
@@ -11,7 +11,7 @@ const searchBooks = async (req, res) => {
             title: { $regex: searchQuery, $options: "i"}
         });
 
-        res.render("availableBooks", { books });
+        res.render("books/availableBooks", { books });
     } catch (err) {
         console.log("Search error:", err);
         res.status(500).send("Server error");
@@ -22,14 +22,71 @@ const searchBooks = async (req, res) => {
 const getAllBooks = async (req, res) => {
     try {
         const books = await Books.find({});
-        res.render("availableBooks", { books });
+        res.render("books/books", { books });
     } catch (err) {
         console.log("Error getting all the books:", err);
         res.status(500).send("Server error");
     }
 };
 
+// Loan a book
+const loanBook = async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    const userId = req.user._id;
+
+    await Books.findByIdAndUpdate(bookId, {
+      available: false,
+      loanedTo: userId
+    });
+
+    res.redirect("/books");
+  } catch (err) {
+    console.error("Loan error:", err);
+    res.status(500).send("Error loaning book");
+  }
+};
+
+
+// Get books loaned to the logged-in user
+const getMyBooks = async (req, res) => {
+  try {
+    const books = await Books.find({ loanedTo: req.user._id });
+    res.render("books/myBooks", { books });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error loading my books");
+  }
+};
+
+
+// Return a book 
+const returnBook = async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    const userId = req.user._id;
+
+    const book = await Books.findOneAndUpdate(
+      { _id: bookId, loanedTo: userId },
+      { available: true, loanedTo: null },
+      { new: true }
+    );
+
+    if (!book) {
+      return res.status(404).send("Book not found or not loaned to you");
+    }
+
+    res.redirect("/books/my-books");
+  } catch (err) {
+    console.error("Return error:", err);
+    res.status(500).send("Error returning book");
+  }
+};
+
 export const booksController = {
     searchBooks,
-    getAllBooks
+    getAllBooks,
+    loanBook,
+    getMyBooks,
+    returnBook
 };
--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { booksController } from "../controllers/booksController";
+import { booksController } from "../controllers/booksController.js";
 
 const router = express.Router();
 
+router.get("/", booksController.getAllBooks);
 router.get("/search", booksController.searchBooks);
 router.get("/available", booksController.getAllBooks);
+router.get("/my-books", booksController.getMyBooks);
+
+router.post("/loan/:id", booksController.loanBook);
+router.post("/return/:id", booksController.returnBook);
+
 
 export default router;
